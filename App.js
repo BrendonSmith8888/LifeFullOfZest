73c973f67df77040aae04b659af866600b3620f7
Brendon Smith <brendonsmith8888@gmail.com>
2021-11-19 14:23:01 +0200

    Update

---
+++ App.js
@@ -0,0 +1,15 @@
+// import { StatusBar } from 'expo-status-bar';
+import React, { Component } from "react";
+import { WelcomeScreen } from "./src/screens/WelcomeScreen";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <WelcomeScreen />
+    );
+  }
+}
+
+export default App;
